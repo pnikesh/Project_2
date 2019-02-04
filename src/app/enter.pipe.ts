cb48be25ef3fa94f30173b4c1ec4ffdfb3700ca1
Andrei <andrey.laptev@outlook.com>
2019-02-04 14:22:23 -0500

    Clean enter.pipe code

--- src/app/enter.pipe.ts
+++ src/app/enter.pipe.ts
@@ -8,33 +8,24 @@ export class EnterPipe implements PipeTransform {
   transform(flights: any[], search: any, i:number): any {
 
     var filterFlights = [];
-    //let val = (value.departureCity).indexOf(departureCityField); //
-
+  
     var entries = Object.values(search); 
-
-
-
-    
 
     var filteredFlights = flights.filter(flight => {
 
-      //let var1 = flight.departureCity.toLowerCase();
       let var2;
       entries[0] == undefined ? var2 = entries[0] = "" : var2 = entries[0].toLowerCase();
 
       let var3;
       entries[1] == undefined ? var3 = entries[1] = "" : var3 = entries[1].toLowerCase();
-      //let temp = var1.startsWith(var2);
+    
       let temp = flight.departureCity.toLocaleLowerCase().startsWith(var2) && flight.arrivalCity.toLocaleLowerCase().startsWith(var3);
-      return  temp//&&  flight.arrivalCity.toLocaleLowerCase()).startsWith(departureCityField.toLocaleLowerCase());
+      return  temp;
        
     })
    // debugger;
     console.log(filteredFlights);
-    //console.log(i);
-    //if (value.includes(departureCityField))
-
-
+   
 
     return filteredFlights;
   }
