7b60d190faf1bc80ddde0c8d551d9d6db51a3088
Andrei <andrey.laptev@outlook.com>
2019-02-04 14:01:58 -0500

    Departure and arrival cities filter working on Home page

--- src/app/enter.pipe.ts
+++ src/app/enter.pipe.ts
@@ -5,22 +5,37 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EnterPipe implements PipeTransform {
 
-  transform(flights: any[], departureCityField: string, arrivalCityField: string, i:number): any {
+  transform(flights: any[], search: any, i:number): any {
 
     var filterFlights = [];
     //let val = (value.departureCity).indexOf(departureCityField); //
+
+    let entries = Object.values(search); 
+
+
+
+    
+
     var filteredFlights = flights.filter(flight => {
-      return (flight.departureCity.toLocaleLowerCase()).startsWith(departureCityField.toLocaleLowerCase()) //&&  flight.arrivalCity.toLocaleLowerCase()).startsWith(departureCityField.toLocaleLowerCase());
+
+      let var1 = flight.departureCity.toLowerCase();
+      let var2 = entries[0].toLowerCase();
+
+      let var3;
+      entries[1] == undefined ? var3 = entries[1] = "" : var3 = entries[1].toLowerCase();
+      //let temp = var1.startsWith(var2);
+      let temp = flight.departureCity.toLocaleLowerCase().startsWith(entries[0].toLocaleLowerCase()) && flight.arrivalCity.toLocaleLowerCase().startsWith(entries[1].toLocaleLowerCase());
+      return  temp//&&  flight.arrivalCity.toLocaleLowerCase()).startsWith(departureCityField.toLocaleLowerCase());
        
     })
    // debugger;
-    console.log(filterFlights);
+    console.log(filteredFlights);
     //console.log(i);
     //if (value.includes(departureCityField))
 
 
 
-    return filterFlights;
+    return filteredFlights;
   }
 
 }
