6aaf911bd8945466908091d2f124f46cd772b07d
Andrei <bokken2008@gmail.com>
2019-03-06 10:50:37 -0500

    Booking Form added + sylings

--- src/app/flight-details/flight-details.component.ts
+++ src/app/flight-details/flight-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { concat } from 'rxjs/internal/observable/concat';
 declare var $: any;
 
@@ -14,26 +14,35 @@ export class FlightDetailsComponent implements OnInit {
 
 flightId: number;
 flight$: object;
+showForm = false;
 
 
-  constructor(private route: ActivatedRoute, private data: DataService) {
+  constructor(private route: ActivatedRoute, private router: Router, private data: DataService) {
     this.route.params.subscribe( params => this.flightId = params.id );
    }
 
   ngOnInit() {
     this.data.getFlight(this.flightId).subscribe(
-      data => this.flight$ = data          
-    );
-    console.log(this.flight$)
+      data => this.flight$ = data      
 
+    );
     $("button").click(function(){
-      var div = $("div");  
-      console.log('dddddddddd')
-      div.animate({left: '100px'}, "slow");
-      div.animate({fontSize: '5em'}, "slow");
+     // var div = $("div");       
+     // div.animate({left: '100px'}, "slow");
+     // div.animate({fontSize: '5em'}, "slow");
   });
   }
 
+  
+  onChoose() {
+    let chosenFlight = this.flight$;
+    this.showForm = true;
+    console.log(chosenFlight);
+  }
+
+  onDiscard(){
+    this.router.navigate(['/home']);
+  }
  
 
 }
