d4284c16269fc5e0895df39c3dbbbc700bd37b56
Andrei <bokken2008@gmail.com>
2019-03-09 17:10:30 -0500

    Discard button on History page activated

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-history',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onDiscard() {
+
+    this.router.navigate(['home'])
   }
 
 }
