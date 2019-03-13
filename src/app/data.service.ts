a9af7dd3c6bb1ccd932b7444cdec55b082de978c
Andrei <andrey.laptev@outlook.com>
2019-03-12 21:44:36 -0400

    Back to Level 2: enemy's collider changed to Capsule

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -17,6 +17,11 @@ export class DataService {
     return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/flights/'+ flightId)
   }
 
+  getCostomerHistoryByEmail(email){
+    return null;
+  }
+
+
   getVerificationCode(email) {
     return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/customers/code?email=' + email)
     //return this.http.get('https://localhost:44310/api/customers/code?email=' + email) 
