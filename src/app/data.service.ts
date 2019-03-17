2bc51f912924959b785c8de1cbc45f235d1da995
Andrei <andrey.laptev@outlook.com>
2019-03-17 11:50:34 -0400

    Customer's history method added to data.service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -26,4 +26,8 @@ export class DataService {
     return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/customers/code?email=' + email)
     //return this.http.get('https://localhost:44310/api/customers/code?email=' + email) 
   }
+
+  getCustomerHistory(email){
+    return this.http.get('http://laptevphamproject-prod.us-east-1.elasticbeanstalk.com/api/tickets/ticketslist?email=' + email)
+  }
 }
