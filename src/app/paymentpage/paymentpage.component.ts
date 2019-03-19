92ab439e5342e2bcc67dfdd6b2fb9e9f8606f063
kranthi kumar <kranthiveer7@gmail.com>
2019-03-19 02:53:22 -0400

    payment:restricted input fields to maximum numbers

--- src/app/paymentpage/paymentpage.component.ts
+++ src/app/paymentpage/paymentpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-paymentpage',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onpay(){
+    this.router.navigate(['/book_confirmation']);
+  }
+  
 }
+
