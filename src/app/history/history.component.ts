27f162ba7b88a188613cef969525c33727f421a9
Andrei <andrey.laptev@outlook.com>
2019-03-23 11:01:01 -0400

    Confirm email for History page added

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-history',
@@ -8,10 +11,66 @@ import { Router } from '@angular/router';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private data: DataService) { }
+
+  email: String;
+  showForm: boolean = true;
+  showCodeField: boolean = false;
+  isNotValid = false;
+  code: object;
+
+  codeReceived: boolean = false;
+  enteredCode: number;
+  isCodeEntered: boolean = false;
+  codeIncorrect: boolean = false;
+
 
   ngOnInit() {
   }
+
+  onEmailEntered(){
+
+    //debugger
+    if (this.email != "") {
+      this.showForm = false;
+      this.showCodeField = true;
+      
+
+      this.data.getVerificationCode(this.email).subscribe(
+        data => {
+          this.code = data;
+          console.log(this.code); 
+          this.codeReceived = true;
+
+        })
+
+    }else {
+      this.isNotValid = true;
+    }
+
+  }
+
+  onCodeEntered(){
+
+    if (this.enteredCode != undefined ) {
+
+    this.checkCode(this.code, this.enteredCode);
+
+   }else 
+   this.isCodeEntered = true;
+  }
+
+  checkCode(code, enteredCode) {
+
+    if (code.code == enteredCode)
+      //console.log("match")
+      this.router.navigate(['/payment']);
+      else
+      //console.log("not match")
+      this.codeIncorrect = true;
+
+  }
+ 
 
   onDiscard() {
 
